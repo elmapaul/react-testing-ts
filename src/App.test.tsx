6bf9d4ba772a2a022bdf6cfc56cb4972e5f5b5d0
Elmatsidis Paul <p.elmatsidis@gmail.com>
2020-02-20 17:52:34 +0200

    Started testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16'
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({adapter: new EnzymeAdapter()});
+
+test('renders without warning', () => {
+  const wrapper = shallow(<App />)
+  console.log(wrapper.debug())
+
+  const appComponent = wrapper.find("[data-test='component-app']")
+  expect(appComponent.length).toBe(1);
 });
