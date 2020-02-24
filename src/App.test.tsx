c43c848dbe66dc8cc1a872c7ae64801169654873
Elmatsidis Paul <p.elmatsidis@gmail.com>
2020-02-24 15:21:26 +0200

    Redux tests added

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import EnzymeAdapter from 'enzyme-adapter-react-16'
+import { shallow } from 'enzyme';
 import App from './App';
 
-Enzyme.configure({adapter: new EnzymeAdapter()});
 
 /**
  * Factory function to create a ShallowWrapper for the App component
