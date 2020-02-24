f1b1610236c753eae13e5229f8775f20b75906bd
Elmatsidis Paul <p.elmatsidis@gmail.com>
2020-02-24 13:53:50 +0200

    Add bootstrap layout and getLetterMatchCount helper function

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -29,36 +29,36 @@ const findByTestAttr = (wrapper, val) => {
 };
 
 test('renders without warning', () => {
-  const wrapper = setup();
-  console.log(wrapper.debug());
-
-  const appComponent = findByTestAttr(wrapper, 'component-app');
-  expect(appComponent.length).toBe(1);
+  // const wrapper = setup();
+  // console.log(wrapper.debug());
+  //
+  // const appComponent = findByTestAttr(wrapper, 'component-app');
+  // expect(appComponent.length).toBe(1);
 });
-test('renders without warning', () => {
-  const wrapper = setup();
-  const button = findByTestAttr(wrapper, 'increment-button');
-  expect(button.length).toBe(1);
-});
-test('renders without warning', () => {
-  const wrapper = setup();
-  const counterDisplay = findByTestAttr(wrapper, 'counter-display')
-  expect(counterDisplay.length).toBe(1);
-});
-test('[App] counter starts at 0', () => {
-  const wrapper = setup();
-  const initialCounterState = wrapper.state('counter');
-  expect(initialCounterState).toBe(0);
-});
-test('clicking button increments counter display', () => {
-  const counter = 7;
-  const wrapper = setup(null, { counter });
-
-  // find button and click
-  const button = findByTestAttr(wrapper, 'increment-button');
-  button.simulate('click');
-
-  // find display and test value
-  const counterDisplay = findByTestAttr(wrapper, 'counter-display');
-  expect(counterDisplay.text()).toContain(counter + 1);
-})
+// test('renders without warning', () => {
+//   const wrapper = setup();
+//   const button = findByTestAttr(wrapper, 'increment-button');
+//   expect(button.length).toBe(1);
+// });
+// test('renders without warning', () => {
+//   const wrapper = setup();
+//   const counterDisplay = findByTestAttr(wrapper, 'counter-display')
+//   expect(counterDisplay.length).toBe(1);
+// });
+// test('[App] counter starts at 0', () => {
+//   const wrapper = setup();
+//   const initialCounterState = wrapper.state('counter');
+//   expect(initialCounterState).toBe(0);
+// });
+// test('clicking button increments counter display', () => {
+//   const counter = 7;
+//   const wrapper = setup(null, { counter });
+//
+//   // find button and click
+//   const button = findByTestAttr(wrapper, 'increment-button');
+//   button.simulate('click');
+//
+//   // find display and test value
+//   const counterDisplay = findByTestAttr(wrapper, 'counter-display');
+//   expect(counterDisplay.text()).toContain(counter + 1);
+// })
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
 
+import GuessedWords from './GuessedWords';
+import Congrats from './Congrats';
+
 import './App.css';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            counter: 0
-        }
-    }
+    // constructor(props) {
+    //     super(props);
+    //
+    //     this.state = {
+    //         counter: 0
+    //     }
+    // }
 
     render() {
         return (
-            <div data-test="component-app">
-                <h1 data-test="counter-display"> The counter is currently {this.state.counter}</h1>
-                <button
-                    data-test="increment-button"
-                    onClick={()=> this.setState({counter: this.state.counter + 1})}
-                >
-                    Increment Counter
-                </button>
+            <div className="container">
+                <h1>Jotto</h1>
+                <Congrats success={true} />
+                <GuessedWords guessedWords={[
+                    { guessedWord: 'train', letterMatchCount: 3}
+                ]} />
             </div>
         );
     }
