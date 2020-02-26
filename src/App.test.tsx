9b9d1ddaa2b5585a777f29b82017f589f82d3d41
Elmatsidis Paul <p.elmatsidis@gmail.com>
2020-02-26 12:46:32 +0200

    Connect App component (and tests)

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,63 +1,45 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import {shallow} from 'enzyme';
+import {storeFactory} from '../test/testUtils';
 import App from './App';
 
-
 /**
- * Factory function to create a ShallowWrapper for the App component
  * @function setup
- * @param {object} props - Component props specific to this setup
  * @param {object} state - Initial state for setup
  * @returns {ShallowWrapper}
  */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const setup = (props={}, state=null) => {
-  const wrapper = shallow(<App {...propss} />);
-  if (state) wrapper.setState(state);
-  return wrapper;
+const setup = (state={}) => {
+  const store = storeFactory(state);
+  return shallow(<App store={store}/>).dive().dive();
 };
 
-/**
- * Return ShallowWrapper containing node(s) with the given data-test value
- * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search within
- * @param {string} val - Value of data-test attribute for search
- * @returns {ShallowWrapper}
- */
-const findByTestAttr = (wrapper, val) => {
-  return wrapper.find(`[data-test="${val}"]`);
-};
-
-test('renders without warning', () => {
-  // const wrapper = setup();
-  // console.log(wrapper.debug());
-  //
-  // const appComponent = findByTestAttr(wrapper, 'component-app');
-  // expect(appComponent.length).toBe(1);
+describe('redux properties', () => {
+  test('has access to `success` state', ()=>{
+    const success = true;
+    const wrapper = setup({success});
+    // @ts-ignore
+    const successProp = wrapper.instance().props.success
+    expect(successProp).toBe(success);
+  });
+  test('has access to `secretWord` state', ()=>{
+    const secretWord = 'party';
+    const wrapper = setup({secretWord})
+    // @ts-ignore
+    const secretWordProp = wrapper.instance().props.secretWord;
+    expect(secretWordProp).toBe(secretWord);
+  });
+  test('has access to `guessedWords` state', ()=>{
+    const guessedWords = [ { guessedWord: 'train', letterMatchCount: 3}];
+    const wrapper = setup({ guessedWords });
+    // @ts-ignore
+    const guessedWordsProp = wrapper.instance().props.guessedWords;
+    expect(guessedWordsProp).toEqual(guessedWords)
+  });
+  test('`getSecretWord` action creator is a function on the props', ()=>{
+    const wrapper = setup();
+    // @ts-ignore
+    const getSecretWordProp = wrapper.instance().props.getSecretWord;
+    expect(getSecretWordProp).toBeInstanceOf(Function);
+  });
 });
-// test('renders without warning', () => {
-//   const wrapper = setup();
-//   const button = findByTestAttr(wrapper, 'increment-button');
-//   expect(button.length).toBe(1);
-// });
-// test('renders without warning', () => {
-//   const wrapper = setup();
-//   const counterDisplay = findByTestAttr(wrapper, 'counter-display')
-//   expect(counterDisplay.length).toBe(1);
-// });
-// test('[App] counter starts at 0', () => {
-//   const wrapper = setup();
-//   const initialCounterState = wrapper.state('counter');
-//   expect(initialCounterState).toBe(0);
-// });
-// test('clicking button increments counter display', () => {
-//   const counter = 7;
-//   const wrapper = setup(null, { counter });
-//
-//   // find button and click
-//   const button = findByTestAttr(wrapper, 'increment-button');
-//   button.simulate('click');
-//
-//   // find display and test value
-//   const counterDisplay = findByTestAttr(wrapper, 'counter-display');
-//   expect(counterDisplay.text()).toContain(counter + 1);
-// })
